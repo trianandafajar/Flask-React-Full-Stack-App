66ceb9e72173d052dcc08df0411971dd90b9889c
trianandafajar <mail.trianandafajar@gmail.com>
2025-04-05 10:55:18 +0700

    Update APP

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,55 +1,74 @@
 import { useState, useEffect } from "react";
 import ContactList from "./ContactList";
-import "./App.css";
 import ContactForm from "./ContactForm";
+import "./App.css";
 
 function App() {
   const [contacts, setContacts] = useState([]);
-  const [isModalOpen, setIsModalOpen] = useState(false)
-  const [currentContact, setCurrentContact] = useState({})
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [currentContact, setCurrentContact] = useState({});
 
   useEffect(() => {
-    fetchContacts()
+    fetchContacts();
   }, []);
 
   const fetchContacts = async () => {
-    const response = await fetch("http://127.0.0.1:5000/contacts");
-    const data = await response.json();
-    setContacts(data.contacts);
+    try {
+      const response = await fetch("http://127.0.0.1:5000/contacts");
+      const data = await response.json();
+      setContacts(data.contacts);
+    } catch (error) {
+      console.error("Failed to fetch contacts:", error);
+    }
   };
 
   const closeModal = () => {
-    setIsModalOpen(false)
-    setCurrentContact({})
-  }
+    setIsModalOpen(false);
+    setCurrentContact({});
+  };
 
   const openCreateModal = () => {
-    if (!isModalOpen) setIsModalOpen(true)
-  }
+    if (!isModalOpen) setIsModalOpen(true);
+  };
 
   const openEditModal = (contact) => {
-    if (isModalOpen) return
-    setCurrentContact(contact)
-    setIsModalOpen(true)
-  }
+    if (isModalOpen) return;
+    setCurrentContact(contact);
+    setIsModalOpen(true);
+  };
 
   const onUpdate = () => {
-    closeModal()
-    fetchContacts()
-  }
+    closeModal();
+    fetchContacts();
+  };
 
   return (
-    <>
-      <ContactList contacts={contacts} updateContact={openEditModal} updateCallback={onUpdate} />
-      <button onClick={openCreateModal}>Create New Contact</button>
-      {isModalOpen && <div className="modal">
-        <div className="modal-content">
-          <span className="close" onClick={closeModal}>&times;</span>
-          <ContactForm existingContact={currentContact} updateCallback={onUpdate} />
+    <div className="app-container">
+      <h1>Contact Manager</h1>
+      <ContactList
+        contacts={contacts}
+        updateContact={openEditModal}
+        updateCallback={onUpdate}
+      />
+
+      <button className="modern-button" onClick={openCreateModal}>
+        + Add New Contact
+      </button>
+
+      {isModalOpen && (
+        <div className="modal">
+          <div className="modal-content">
+            <span className="close" onClick={closeModal}>
+              &times;
+            </span>
+            <ContactForm
+              existingContact={currentContact}
+              updateCallback={onUpdate}
+            />
+          </div>
         </div>
-      </div>
-      }
-    </>
+      )}
+    </div>
   );
 }
 
